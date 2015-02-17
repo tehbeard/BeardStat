8377c962083126733c117390413fd2628f0e28f3
James <beardyjames@live.co.uk>
2015-02-17 23:21:35 +0000

    Add armor to document.

--- src/main/java/com/tehbeard/beardstat/bukkit/documents/CharacterSheetDocument.java
+++ src/main/java/com/tehbeard/beardstat/bukkit/documents/CharacterSheetDocument.java
@@ -5,11 +5,18 @@
  */
 package com.tehbeard.beardstat.bukkit.documents;
 
+import com.google.common.collect.HashBiMap;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 import com.tehbeard.beardstat.containers.EntityStatBlob;
 import com.tehbeard.beardstat.containers.documents.IStatDocument;
 import com.tehbeard.beardstat.containers.documents.IStatDynamicDocument;
 import com.tehbeard.beardstat.containers.documents.StatDocument;
 import com.tehbeard.beardstat.containers.documents.docfile.DocumentFile;
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 /**
  *
@@ -17,10 +24,21 @@ import com.tehbeard.beardstat.containers.documents.docfile.DocumentFile;
  */
 @StatDocument(value = "CharSheet",singleInstance = true)
 public class CharacterSheetDocument implements IStatDynamicDocument {
+    
+    @Expose
+    Map<String,String> armor = new HashMap<String, String>();
+    
+    Player player = null;
 
     @Override
     public void updateDocument(EntityStatBlob blob) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(player != null) {
+            ItemStack[] am = player.getInventory().getArmorContents();
+            armor.put("head", am[0].getType().toString());
+            armor.put("chest", am[1].getType().toString());
+            armor.put("legs", am[2].getType().toString());
+            armor.put("feet", am[3].getType().toString());
+        }
     }
 
     
