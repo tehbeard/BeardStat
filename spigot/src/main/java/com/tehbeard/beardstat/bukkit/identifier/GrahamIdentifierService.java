23311057964dcf280c84d11ed69219b3362cf2e2
James <beardyjames@live.co.uk>
2016-01-08 22:49:19 +0000

    Add entity data to plugin.

--- spigot/src/main/java/com/tehbeard/beardstat/bukkit/identifier/GrahamIdentifierService.java
+++ spigot/src/main/java/com/tehbeard/beardstat/bukkit/identifier/GrahamIdentifierService.java
@@ -23,11 +23,16 @@ import org.bukkit.potion.PotionEffect;
 public class GrahamIdentifierService implements IIdentifierGenerator {
 
     private static Map<String, GrahamItem> items;
+    
+    private static Map<String, GrahamEntity> entities;
 
     public static void readData() {
         //Load item data
         Gson gson = new Gson();
         List<GrahamItem> itemList = gson.fromJson(new InputStreamReader(GrahamIdentifierService.class.getResourceAsStream("data/items.json")), new TypeToken<List<GrahamItem>>() {
+        }.getType());
+        
+        List<GrahamEntity> entityList = gson.fromJson(new InputStreamReader(GrahamIdentifierService.class.getResourceAsStream("data/entities.json")), new TypeToken<List<GrahamEntity>>() {
         }.getType());
 
         for (GrahamItem i : itemList) {
@@ -60,6 +65,16 @@ public class GrahamIdentifierService implements IIdentifierGenerator {
     public class GrahamItem {
         public int type;
         public int meta;
+        public String name;
+        public String text_type;
+
+        public String getKey() {
+            return "minecraft:" + text_type;
+        }
+    }
+    
+    public class GrahamEntity {
+        public int type;
         public String name;
         public String text_type;
 
