d31923f31057ab6b408889065dd4e58aefbd6ced
Tehbeard <tehbeard@gmail.com>
2019-08-15 10:46:12 +0100

    Testing building settings.xml from node process

---
+++ .github/setup-mvn-settings.js
@@ -0,0 +1,17 @@
+
+const settingsContent = `<settings>
+   <servers>
+     <server>
+       <id>dev-repo</id>
+       <username>maven</username>
+       <password>${process.env.MVN_PASS}</password>
+     </server>
+     <server>
+       <id>dev-repo-snap</id>
+       <username>maven</username>
+       <password>${process.env.MVN_PASS}</password>
+     </server>
+   </servers>
+ </settings>`
+
+console.log(settingsContent)
